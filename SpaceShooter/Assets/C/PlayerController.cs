3bd23f0e2b1b967ad330152739a91604e8bf9666
NEW\Miki <nattakan.eimo@bumail.net>
2017-03-18 12:35:26 +0700

    Clip 5 Done

--- SpaceShooter/Assets/C/PlayerController.cs
+++ SpaceShooter/Assets/C/PlayerController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float moveSpeed;
+	float mhorizontal = Input.GetAxis("Horizontal");
+	float mvertical = Input.GetAxis("Vertical");
+
 	// Use this for initialization
 	void Start () {
-		
+		GetComponent<Rigidbody>().position = Vector3.zero;
+		moveSpeed = 10f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate (moveSpeed*mhorizontal*Time.deltaTime,0f,moveSpeed*mvertical*Time.deltaTime);
+		//rigidbody.position = new Vector3 (
+			//Mathf.Clamp (rigidbody.postion.x, xMin, xMax)
+				//, 0.0f
+				//, Mathf.Clamp (rigidbody.postion.z, zMin, zMax)
+		//);
 	}
 }
